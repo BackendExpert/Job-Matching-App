6788054d83cb37b957a57f68b58d679506ef81af
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 15:34:09 +0530

    Update DashSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Dashboard/DashSide.jsx
+++ client/src/components/Dashboard/DashSide.jsx
@@ -19,29 +19,41 @@ const DashSide = () => {
       setIsOpen(!isOpen);
     };
 
-  return (
-    <div className=''>
-        <button className="md:hidden fixed top-4 right-4 z-50 bg-gray-600 text-white p-2 rounded font-semibold" onClick={toggleSidebar}>
-            {
-                !isOpen ? <BsList /> : <BsX />
-            }
-        </button>
-
-        <div className={`rounded shadow-md md:mt-4 py-4 px-4 md:min-w-64 md:max-w-64 bg-white w-full md:w-auto md:min-h-screen md:relative fixed overflow-auto top-0 left-0 h-full w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
-            <div className="">
-                <div class="w-full flex justify-center">
-                    <img src="https://cdn-icons-png.flaticon.com/128/847/847969.png" alt="" className='h-32 w-auto rounded-full' />                           
-                </div>
-                <div className="text-center text-gray-500">
-                    <h1 className='mt-4 font-semibold text-xl'>Jehan Weerasuriya</h1>
-                    <p className="text-sm">Full Stack Developer</p>
-                </div>
-
-            </div>
-        </div>
+    if(RoleUser !== null && EmailUser !== null){
+        return (
+            <div className=''>
+                <button className="md:hidden fixed top-4 right-4 z-50 bg-gray-600 text-white p-2 rounded font-semibold" onClick={toggleSidebar}>
+                    {
+                        !isOpen ? <BsList /> : <BsX />
+                    }
+                </button>
         
-    </div>
-  )
+                <div className={`rounded shadow-md md:mt-4 py-4 px-4 md:min-w-64 md:max-w-64 bg-white w-full md:w-auto md:min-h-screen md:relative fixed overflow-auto top-0 left-0 h-full w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
+                    <div className="">
+                        <div class="w-full flex justify-center">
+                            <img src="https://cdn-icons-png.flaticon.com/128/847/847969.png" alt="" className='h-32 w-auto rounded-full' />                           
+                        </div>
+                        <div className="text-center text-gray-500">
+                            <h1 className='mt-4 font-semibold text-xl'>Jehan Weerasuriya</h1>
+                            <p className="text-sm">Full Stack Developer</p>
+                        </div>
+                        <hr className='my-2'/>
+        
+                        <div className="">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi inventore alias blanditiis asperiores at hic ad assumenda. Assumenda iure nisi necessitatibus laboriosam ratione? Ipsum non facilis repellat labore quam aut.
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+          )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default DashSide
