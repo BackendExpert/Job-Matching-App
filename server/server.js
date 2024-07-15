c6aa04fd58e33e37e34f2a44bd9245aa57d85eb2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 20:05:03 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 
 const AuthRoute = require('./Routes/AuthRoute')
+const JobFinderRoute = require('./Routes/JobFinderRoute')
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -18,6 +19,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/auth', AuthRoute)
+app.use('/jobfinder', JobFinderRoute)
 
 app.listen(PORT, () => {
     console.log(`Server Running on PORT ${PORT}`)
