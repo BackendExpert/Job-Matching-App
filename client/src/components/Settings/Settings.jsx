4075313215ab14a32ef669f81e65e53f03aa9165
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 19:05:12 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import MyImg from '../../assets/file.jpg'
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
 
 const Settings = () => {
-  return (
-    <div>Settings</div>
-  )
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(RoleUser !== null && EmailUser !== null){
+        return (
+            <div className='bg-white py-4 px-6 shadow-md rounded'>
+                <h1 className="text-2xl font-semibold text-gray-500 pt-2">Settings</h1>
+                <hr  className='py-2'/>
+        
+                <div className="my-4">
+                    <div className="flex">
+        
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        })
+    }
 }
 
 export default Settings
