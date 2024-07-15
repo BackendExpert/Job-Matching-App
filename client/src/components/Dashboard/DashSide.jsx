2e5960373318252ef912b0840175384e2868dc76
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 15:20:15 +0530

    Updte
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Dashboard/DashSide.jsx
+++ client/src/components/Dashboard/DashSide.jsx
@@ -20,14 +20,14 @@ const DashSide = () => {
     };
 
   return (
-    <div className='bg-white mt-4 py-2 px-8 md:w-56 rounded w-full shadow-md'>
+    <div className=''>
         <button className="md:hidden fixed top-4 right-4 z-50 bg-gray-600 text-white p-2 rounded font-semibold" onClick={toggleSidebar}>
             {
                 !isOpen ? <BsList /> : <BsX />
             }
         </button>
 
-        <div className={`w-full md:w-auto md:min-h-screen md:relative fixed overflow-auto top-0 left-0 h-full w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
+        <div className={`rounded shadow-md md:mt-4 py-4 px-4 md:max-w-64 bg-white w-full md:w-auto md:min-h-screen md:relative fixed overflow-auto top-0 left-0 h-full w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum nostrum eveniet qui deserunt exercitationem dicta impedit autem fuga cum at, quod beatae enim accusamus aliquam! Distinctio illum hic nobis vitae.
         </div>
         
