e1406f5ac82a41e609594a487fc714a1c0052ade
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 16:02:18 +0530

    Create Jobs.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/Jobs/Jobs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Jobs = () => {
+  return (
+    <div className='bg-white py-4 px-6 shadow-md rounded'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolores laboriosam vitae veniam perspiciatis voluptas officiis. Odit, provident officia, adipisci recusandae, laborum eos earum quibusdam sapiente amet eligendi corporis assumenda!
+        Ut perferendis officiis itaque vel dolorem dolores vitae. Ut qui ducimus inventore quos earum ratione nihil commodi adipisci facilis, quia deleniti voluptatibus numquam illo voluptates non! Consectetur numquam architecto facere?
+    </div>
+  )
+}
+
+export default Jobs
