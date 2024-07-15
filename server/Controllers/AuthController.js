c23deef98fa33e6318c51f2284c34bf1e5c5f2c2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 07:46:00 +0530

    Update AuthController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,11 +1,41 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const User = require('../Models/User')
 
 const AuthController = {
     SignUp: async (req, res) => {
         try{
             const {username, email, password} = req.body
 
+            const chechuser = await User.findOne({
+                $and: [
+                    {username: username},
+                    {email: email}
+                ]
+            })
+
+            if(chechuser) {
+                return res.json({ Error: "User Already Exists"})
+            }
+            else{
+                const hashPass = await bcrypt.hash(password, 10)
+
+                const NewUser = new User({
+                    username: username,
+                    email: email,
+                    password: hashPass,
+                    Role: "JobFinder"
+                })
+
+                const ResultUser = NewUser.save()
+
+                if(ResultUser) {
+                    return res.json({ Status: "Success"})
+                }
+                else{
+                    return res.json({ Error: "Internal Server Error"})
+                }
+            }
 
         }
         catch (err){
