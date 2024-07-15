959483915d49aea05fc4e96b80f42a3abced9830
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 14:46:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/Dashboard/DashFooter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashFooter = () => {
+  return (
+    <div>DashFooter</div>
+  )
+}
+
+export default DashFooter
--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import  secureLocalStorage  from  "react-secure-storage"
 import { Outlet } from 'react-router-dom';
+import DashNav from './DashNav';
+import DashSide from './DashSide';
 
 const Dashboard = () => {
     const navigate = useNavigate()
@@ -10,8 +12,15 @@ const Dashboard = () => {
 
     if(RoleUser !== null && EmailUser !== null){
         return (
-            <div className='bg-gray-200 py-8 px-12'>
-
+            <div className="bg-gradient-to-br from-purple-600 to-blue-600 w-full min-h-screen md:px-40 px-4">
+                <DashNav />
+        
+                <div className="md:flex">
+                    <div className="">
+                        <DashSide />
+                    </div>
+                </div>
+                <Footer />
             </div>
         )
     }
