9b81b861f771b8536a9d3dcc1bd6292ad3342391
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 07:36:17 +0530

    Update AuthController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -2,7 +2,16 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const AuthController = {
+    SignIn: async (req, res) => {
+        try{
+            const {username, email, password} = req.body
 
+            
+        }
+        catch (err){
+            console.log(err)
+        }
+    }
 }
 
 module.exports = AuthController
