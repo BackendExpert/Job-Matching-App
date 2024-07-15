24ce058424d761596b6a61b75abe12b7c5ac93ec
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 15:08:41 +0530

    Update DashNav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Dashboard/DashNav.jsx
+++ client/src/components/Dashboard/DashNav.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { BsBriefcaseFill, BsPower } from 'react-icons/bs'
 
 const DashNav = () => {
   return (
     <div className="pt-6">
         <div className='bg-white py-4 px-8 rounded shadow-md'>
-            <div className="flex">
-                <h1 className="text-xl pt-1 pl-2 font-semibold text-gray-500">Jehan Weerasuriya</h1>
+            <div className="flex justify-between">
+                <div className="flex">
+                    <BsBriefcaseFill className='h-8 w-auto text-purple-500'/>
+                    <h1 className="text-xl pt-1 pl-2 font-semibold text-orange-400">Job Finder</h1>
+                </div>
+                <div className="flex">
+                    <BsBriefcaseFill className='h-8 w-auto text-purple-500'/>
+                    <h1 className="text-xl pt-1 pl-2 font-semibold text-orange-400">Job Finder</h1>
+                </div>
+                <div className="flex">
+                    <BsPower className='h-4 w-auto text-red-500 mt-2'/>
+                    <h1 className="pt-1 text-red-400">Logout</h1>
+                </div>
             </div>
         </div>
     </div>
