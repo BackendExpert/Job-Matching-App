5d57fc137c7f7fc5eb22ed585947f0216df46132
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 20:13:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/JobFinderController.js
+++ server/Controllers/JobFinderController.js
@@ -1,7 +1,23 @@
 const JobFinder = require('../Models/JobFinder')
 
 const JobFinderController = {
+    GetDataJF: async (req, res) => {
+        try{
+            const UserID = req.params.UserID
 
+            const JFData = await JobFinder.findOne({ email: UserID })
+
+            if(JFData){
+                return res.json({ Result: JFData })
+            }
+            else{
+                return res.json({ Error: "Internal Server Error"})
+            }
+        }
+        catch (err){
+            console.log(err)
+        }
+    }
 }
 
 module.exports = JobFinderController
--- server/Routes/JobFinderRoute.js
+++ server/Routes/JobFinderRoute.js
@@ -2,4 +2,6 @@ const express = require('express');
 
 const router = express.Router()
 
+router.get('/GetJFData/:id', )
+
 module.exports = router
