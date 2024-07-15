665a6520ce135fd3930658249b5d8eddf3eac34d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 18:24:59 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Dashboard/DashHome.jsx
+++ client/src/components/Dashboard/DashHome.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import  secureLocalStorage  from  "react-secure-storage"
+import { Link, useNavigate } from 'react-router-dom';
 
 const DashHome = () => {
-  return (
-    <div className='bg-white py-4 px-6 shadow-md rounded'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga pariatur ipsa esse aliquid possimus natus, est odio deleniti. Dolorum quia rerum vitae facilis, dolor minus modi suscipit libero voluptas ab.
-        Nulla dolor eos minus velit dicta quibusdam laudantium provident aspernatur, ab delectus obcaecati, animi quam, fuga alias molestiae odit minima deleniti natus repudiandae mollitia fugiat at ipsum? Quasi, recusandae alias!
-        Quisquam, perspiciatis? Minima voluptate provident quidem error impedit eveniet quasi, aliquam aut adipisci. Officia perferendis cumque voluptate laudantium magnam a possimus, consectetur, ratione nostrum ipsum labore quisquam, est ea aliquam!
-        Tenetur quaerat accusantium, eveniet modi recusandae exercitationem. Recusandae nostrum, quas facere optio alias nisi incidunt impedit dolores fugiat voluptatum harum, laudantium deserunt ab eum facilis eaque, adipisci perferendis saepe voluptate.
-        Eaque voluptatem vitae animi enim placeat fuga quidem, eius architecto incidunt ut ab, alias molestias, magnam nesciunt quam tempora ullam est amet sit eligendi iusto. Harum soluta laudantium natus libero?
-        Maiores ex, adipisci sed assumenda natus optio itaque reprehenderit ratione! Magnam laborum ut, cupiditate, aut perferendis officia quidem, tempora repellat tempore autem sapiente assumenda! Et impedit voluptas velit cumque? Eligendi?
-        Eos ab quas amet unde temporibus recusandae quia similique dolorum. Iusto fugiat excepturi ipsum facere deserunt blanditiis perferendis praesentium exercitationem saepe consequuntur eius iste harum voluptate aut illum, aliquid libero.
-        Repellat aut aperiam rem est, deserunt, in ducimus repudiandae dolorem recusandae ipsum commodi. Accusamus odio illo a minima quaerat? Quae aut est labore expedita distinctio fuga incidunt corporis. Consequuntur, nam.
-        Reiciendis, libero! Ad delectus doloribus recusandae laborum sapiente perferendis, quod repellendus placeat deserunt repudiandae possimus, officia maxime quisquam vel quae velit a nesciunt nobis totam enim, culpa nostrum quis assumenda.
-        Officiis, similique ea. Laborum, illo voluptatibus reprehenderit dolor, nisi odit eum laboriosam dolore unde enim error provident itaque animi. Cumque distinctio natus neque deserunt placeat aspernatur quisquam suscipit totam. Odit.
-        Eius nulla dolorem assumenda iure, eligendi nam amet suscipit pariatur cupiditate enim dolor minima architecto in asperiores numquam excepturi unde odio aliquid dolores alias ab quia nemo dolore atque? Quisquam!
-        Magnam illo repellat aliquid est? Similique consequatur iste nulla dolor, expedita aliquid tempora eveniet dicta dolorem aperiam sint maxime? Facilis culpa necessitatibus similique quia aliquid soluta obcaecati corrupti tempore voluptatum!
-    </div>
-  )
+  const navigate = useNavigate()
+  const EmailUser = secureLocalStorage.getItem("Login1");
+  const RoleUser = secureLocalStorage.getItem("Login2");
+  
+  if(RoleUser === null && EmailUser === null){
+    return (
+      <div className='bg-white py-4 px-6 shadow-md rounded'>
+  
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear()
+      navigate('/')
+    })
+  }
 }
 
 export default DashHome
