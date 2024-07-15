d96b6583a32d4d0f3eb061859d641dc0451f5e17
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-15 15:30:16 +0530

    Update DashSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Dashboard/DashSide.jsx
+++ client/src/components/Dashboard/DashSide.jsx
@@ -27,8 +27,17 @@ const DashSide = () => {
             }
         </button>
 
-        <div className={`rounded shadow-md md:mt-4 py-4 px-4 md:max-w-64 bg-white w-full md:w-auto md:min-h-screen md:relative fixed overflow-auto top-0 left-0 h-full w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum nostrum eveniet qui deserunt exercitationem dicta impedit autem fuga cum at, quod beatae enim accusamus aliquam! Distinctio illum hic nobis vitae.
+        <div className={`rounded shadow-md md:mt-4 py-4 px-4 md:min-w-64 md:max-w-64 bg-white w-full md:w-auto md:min-h-screen md:relative fixed overflow-auto top-0 left-0 h-full w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
+            <div className="">
+                <div class="w-full flex justify-center">
+                    <img src="https://cdn-icons-png.flaticon.com/128/847/847969.png" alt="" className='h-32 w-auto rounded-full' />                           
+                </div>
+                <div className="text-center text-gray-500">
+                    <h1 className='mt-4 font-semibold text-xl'>Jehan Weerasuriya</h1>
+                    <p className="text-sm">Full Stack Developer</p>
+                </div>
+
+            </div>
         </div>
         
     </div>
