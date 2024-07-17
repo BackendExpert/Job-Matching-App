0c61efd8a89f6d83af642064de5e61326b74cbdb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-17 11:04:36 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 import DashNav from './DashNav';
 import DashSide from './DashSide';
 import DashFooter from './DashFooter';
-import { BsChatDotsFill, BsXCircleFill } from 'react-icons/bs';
+import { BsChatDotsFill, BsHeadset, BsXCircleFill } from 'react-icons/bs';
 
 const Dashboard = () => {
     const navigate = useNavigate()
@@ -22,6 +22,12 @@ const Dashboard = () => {
         SetchatOpen(false)
     }
     
+    const ChatMenu = [
+        {id: 1, name: 'How to Start a Company'},
+        {id: 2, name: 'How Find a Job'},
+        {id: 3, name: 'How to add Skills or Project'},
+    ]
+
     if(RoleUser !== null && EmailUser !== null){
         return (
             <div className=" w-full min-h-screen md:px-[5%] px-4">
@@ -33,13 +39,33 @@ const Dashboard = () => {
                     </div>
                     <div className="my-4 md:ml-4 w-full h-auto">                  
                         <Outlet />
-                        <button className="fixed bottom-6 right-8 p-3">
+                        <button className="fixed bottom-2 right-8 p-3">
                             {
                                 chatOpen === true ?
                                     <BsXCircleFill className='h-8 w-auto text-purple-500' onClick={headleCloseChat}/>
                                 :
                                     <BsChatDotsFill className='h-8 w-auto text-purple-500' onClick={headleOpenChat}/>    
                             }
+
+                            <div className={`md:max-w-[20%] max-w-[80%] fixed bottom-16 right-4 bg-purple-800 text-white p-6 rounded-lg transition-transform duration-300 ${chatOpen ? 'transform translate-x-0' : 'transform translate-x-full mr-[-20px]'}`}>
+                                <div className="">
+                                    <div className="pb-4 flex">
+                                        <BsHeadset className='pl-4 h-6 w-auto'/>
+                                        <h1 className="pl-2">Help Assistant</h1>
+                                    </div>
+                                </div>
+                                <div className="text-purple-500 rounded shadow-md">
+                                    {
+                                        ChatMenu.map((menuChat, index) => {
+                                            return (
+                                                <div className="bg-white p-2 my-2 rounded w-full" key={index}>
+                                                    <p className="text-left text-sm">{menuChat.name}</p>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
                             
                         </button>
                     </div>                    
