d141c2a9a34e8e71089712af2db67c9f88d3884c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-17 17:53:02 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Routes/JobFinderRoute.js
+++ server/Routes/JobFinderRoute.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const JobFinderController = require('../Controllers/JobFinderController');
+const multer = require('multer');
+const path = require('path');
 
 const router = express.Router()
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+    },
+});
+
 
 router.get('/GetJFData/:id', JobFinderController.GetDataJF)
 router.post('/UpdateJFData/:id', JobFinderController.JobFinderUpdate)
