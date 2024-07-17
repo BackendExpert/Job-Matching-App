5c1d9616ee0c6049962bd3249b2e02b6d4780c09
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-17 07:43:42 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUpTW/SignUp.jsx
+++ client/src/components/LoginSignUpTW/SignUp.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { BsMortarboardFill } from "react-icons/bs";
+import { BsMortarboardFill, BsPersonFill, BsPersonPlusFill } from "react-icons/bs";
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios'
+import BgImg from '../../assets/Image.jpg'
 
 const SignUp = () => {
     const navigate = useNavigate()
@@ -35,43 +36,48 @@ const SignUp = () => {
         }
 
     }
+    const styles = {
+        background: `url(${BgImg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }
   return (
-    <div className='bg-gray-200 min-h-screen py-24 px-8'>
+    <div style={styles} className='bg-gray-200 min-h-screen py-24 px-8'>
         <div className="md:grid grid-cols-3 gap-2">
             <div className=""></div>
-            <div className="">
-                <div className="bg-white py-16 px-8 rounded shadow-md w-full ">
-                    <center className='text-gray-500'>
-                        {/* change the Icon According to your needs */}
-                        <h1 className=''><BsMortarboardFill className='h-20 w-auto'/></h1>
-                        <p className="pt-4 text-2xl">Welcome to</p>
-                        <p className="">Your Project Name</p>
+            <div className="md:mx-12"> 
+                <div className="bg-gradient-to-br from-fuchsia-50 to-pink-200 rounded-2xl shadow-md w-full py-4 md:px-0 px-4">
+                    <center className='text-purple-500 my-4'>
+                        <div className="">
+                            <BsPersonPlusFill className='h-16 w-auto rounded-full'/>
+                        </div>
+                        <h1 className="text-xl font-semibold pb-6">Registation</h1>
                     </center>
                     <hr className='my-2'/>
                     <div className="my-4">
                         <form onSubmit={headleSubmit}>
                              <div className="my-2 md:mx-8">
-                                <label htmlFor="" className=''>Username : </label>
-                                <input type="text" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Username'
+                                <label htmlFor="" className='text-purple-500 font-semibold'>Username : </label>
+                                <input type="text" name="" id="" className="w-full h-12 pl-2 rounded bg-purple-300 text-white placeholder-white" required placeholder='Enter Username'
                                 onChange={e => SetSignUpData({...SignUpData, username:e.target.value})}/>
                             </div>
                             <div className="my-2 md:mx-8">
-                                <label htmlFor="" className=''>Email : </label>
-                                <input type="email" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Email Address'
+                                <label htmlFor="" className='text-purple-500 font-semibold'>Email : </label>
+                                <input type="email" name="" id="" className="w-full h-12 pl-2 rounded bg-purple-300 text-white placeholder-white" required placeholder='Enter Email Address'
                                 onChange={e => SetSignUpData({...SignUpData, email:e.target.value})}/>
                             </div>
                             <div className="my-2 md:mx-8">
-                                <label htmlFor="" className=''>Password : </label>
-                                <input type="password" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Password' 
+                                <label htmlFor="" className='text-purple-500 font-semibold'>Password : </label>
+                                <input type="password" name="" id="" className="w-full h-12 pl-2 rounded bg-purple-300 text-white placeholder-white" required placeholder='Enter Password' 
                                 onChange={e => SetSignUpData({...SignUpData, password:e.target.value})}/>
                             </div>
                             <div className="my-2 md:mx-8">
-                                <button type='submit' className='mt-8 font-semibold w-full py-4 px-8 rounded bg-blue-500 text-white shadow-md duration-500 hover:bg-blue-600'>Sign Up</button>
+                                <button type='submit' className='mt-8 font-semibold w-full py-4 px-8 rounded bg-purple-500 text-white shadow-md duration-500 hover:bg-purple-600'>Sign Up</button>
                             </div>
                         </form>
                     </div>
                     <hr className='my-2'/>
-                    <p className="my-4">Already have an Account ? <Link to={'/'}><span className="text-blue-500">SignUp</span></Link></p>
+                    <p className="my-4 pl-4 text-purple-500">Already have an Account ? <Link to={'/'}><span className="text-purple-800">SignIn</span></Link></p>
                 </div>  
             </div>
             <div className=""></div>
