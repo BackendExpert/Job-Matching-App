5a82e2c3481aa8c1486af6a09c44147f6bbe7fbd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-18 11:32:12 +0530

    Create EducationController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ server/Controllers/EducationController.js
@@ -0,0 +1,14 @@
+const Education = require('../Models/Education')
+
+const EducationController = {
+    AddNewEducation: async (req, res) => {
+        try{
+
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+}
+
+module.exports = EducationController
