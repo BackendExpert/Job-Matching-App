940dbb234f1c040face59bc1bbb5fe06aed5e61f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-18 11:37:49 +0530

    Update EducationController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/EducationController.js
+++ server/Controllers/EducationController.js
@@ -3,8 +3,32 @@ const Education = require('../Models/Education')
 const EducationController = {
     AddNewEducation: async (req, res) => {
         try{
-            const EmailId = req.params.EmailId
-            console.log(req.body, EmailId)
+            const EmailId = req.params.id
+            // console.log(req.body, EmailId)
+            const {
+                school,
+                course,
+                classCourese,
+                startDate,
+                endDate
+            } = req.body
+
+            const AddEduNew = new Education({
+                school: school,
+                course: course,
+                classCourse: classCourese,
+                startData: startDate,
+                endData: endDate
+            })
+
+            const ResultNewEdu = AddEduNew.save()
+
+            if(ResultNewEdu){
+                return res.json({ Status: "Success"})
+            }
+            else{
+                return res.json({ Error: "Internal Server Error"})
+            }
         }
         catch(err){
             console.log(err)
