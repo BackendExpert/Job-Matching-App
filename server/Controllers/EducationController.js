880767af51bc834b0e3bb564fbfbd509e48b6397
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-18 11:33:43 +0530

    Update EducationController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/EducationController.js
+++ server/Controllers/EducationController.js
@@ -3,7 +3,8 @@ const Education = require('../Models/Education')
 const EducationController = {
     AddNewEducation: async (req, res) => {
         try{
-
+            const EmailId = req.params.EmailId
+            console.log(req.body, EmailId)
         }
         catch(err){
             console.log(err)
