48b998eea06b0ac60eca6232ea5bee560377a2fd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-20 12:13:00 +0530

    Update Company.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Models/Company.js
+++ server/Models/Company.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 
 const CompanySchema = new mongoose.Schema({
-  
+  comName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  comEmail: {
+    type: String,
+    unique: true,
+  },
+  comAddress: {
+    type: String,
+  },
+  comMobile: {
+    type: String,
+  },
+  Owner:{
+    type: String,
+    required: true,
+    unique: true,
+  }
 });
 
 const Company = mongoose.model('Company', CompanySchema);
