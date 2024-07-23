6fbed39bfe06170a6a1ece798791dd58b40c9b48
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-23 15:03:54 +0530

    Update CompanyRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Routes/CompanyRoute.js
+++ server/Routes/CompanyRoute.js
@@ -4,5 +4,6 @@ const CompanyController = require('../Controllers/CompanyController');
 const router = express.Router();
 
 router.post('/CreateCompany/:id', CompanyController.CreateCompany)
+router.get('/UserCompany/:id', CompanyController.GetCompany)
 
 module.exports = router;
