b3d638d8c87d7ed7bfc1bbf04641322b5055634f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-23 14:46:54 +0530

    Update CompanyController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/CompanyController.js
+++ server/Controllers/CompanyController.js
@@ -24,7 +24,7 @@ const CompanyController = {
 
             const CheckCompany = await Company.findOne(query)
 
-            if(CheckCompany){
+            if(!CheckCompany){
                 const CompanyCreate = new Company({
                     comName: comName,
                     comEmail: comEmail,
@@ -44,7 +44,7 @@ const CompanyController = {
 
             }
             else{
-                return  res.json({ Error: "Internal Server Error"})
+                return  res.json({ Error: "You already have a Company"})
             }
 
         } 
@@ -52,6 +52,24 @@ const CompanyController = {
             console.log(err);
         }
     },
+
+    GetCompany: async(req, res) => {
+        try{
+            const EmailID = req.params.id
+
+            const GetCompany = await Company.findOne({ Owner: EmailID })
+
+            if(GetCompany){
+                return res.json({ Result: GetCompany })
+            }       
+            else{
+                return res.json({ Error: "Internal Server Error"})
+            }
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
 }
 
 module.exports = CompanyController
