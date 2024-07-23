2b0dbc9511e10834f573117b2ba8eb25363a1621
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-23 09:52:11 +0530

    Update CompanyController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/CompanyController.js
+++ server/Controllers/CompanyController.js
@@ -1,7 +1,13 @@
 const Company = require('../Models/Company')
 
 const CompanyController = {
-
+    CreateCompany: async (req, res) => {
+        try {
+            
+        } catch (err) {
+            console.log(err);
+        }
+    },
 }
 
 module.exports = CompanyController
