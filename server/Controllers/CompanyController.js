5d96d34e912db37fe3c508adf0eebe0c60a66580
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-23 09:54:37 +0530

    Update CompanyController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/CompanyController.js
+++ server/Controllers/CompanyController.js
@@ -3,8 +3,20 @@ const Company = require('../Models/Company')
 const CompanyController = {
     CreateCompany: async (req, res) => {
         try {
+            const EmailID = req.params.id
+           
+            // console.log(req.body)
+
+            const {
+                comName,
+                comEmail,
+                comAddress,
+                comMobile,
+            } = req.body
+
             
-        } catch (err) {
+        } 
+        catch (err) {
             console.log(err);
         }
     },
